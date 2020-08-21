6e9f024d5c116bcd43b0e7027285acb075f5ddd4
magik99 <lukaszmilewski99@gmail.com>
2020-08-22 00:10:48 +0200

    bot-spam honeypot, recaptcha fix test no.1

--- src/components/form/contactForm.tsx
+++ src/components/form/contactForm.tsx
@@ -1,29 +1,33 @@
 import React from 'react'
 
-export function ContactForm (props){
+export function ContactForm(props) {
 
 
-    return (
-        <form action={props.action} method={props.method} id="contact-form" name="contact" data-netlify="true">
- <div className="wrap">
-              <div className="input-row">
-                <label htmlFor="contact-email">E-mail:</label><input type="email" id="contact-email"
-                  name="email" required />
-              </div>
-              <div className="input-row">
-                <label htmlFor="contact-title">Tytuł:</label><input type="text" id="contact-title"
-                  name="title" />
-              </div>
-            </div>
-            <div className="input-row textarea">
-              <label htmlFor="contact-text">Wiadomość</label>
-              <textarea name="text" id="contact-text"  required
-                placeholder="Wiadomość" ></textarea>
-            </div>
-            <div className="wrap">
-              <div data-netlify-recaptcha="true" className="recaptcha input-row"></div>
-              <div className="input-row"><button type="submit">Wyślij</button></div>
-            </div>
-        </form>
-    )
+  return (
+    <form data-netlify="true" action={props.action} method={props.method} id="contact-form" name="contact" data-netlify-recaptcha="true" netlify-honeypot="bot-field">
+      <div>
+        <input type="hidden" name="bot-field" />
++   <input type="hidden" name="form-name" value="contact" />
+      </div>
+      <div className="wrap">
+        <div className="input-row">
+          <label htmlFor="contact-email">E-mail:</label><input type="email" id="contact-email"
+            name="email" required />
+        </div>
+        <div className="input-row">
+          <label htmlFor="contact-title">Tytuł:</label><input type="text" id="contact-title"
+            name="title" />
+        </div>
+      </div>
+      <div className="input-row textarea">
+        <label htmlFor="contact-text">Wiadomość</label>
+        <textarea name="text" id="contact-text" required
+          placeholder="Wiadomość" ></textarea>
+      </div>
+      <div className="wrap">
+        <div data-netlify-recaptcha="true" className="recaptcha input-row"></div>
+        <div className="input-row"><button type="submit">Wyślij</button></div>
+      </div>
+    </form>
+  )
 }
