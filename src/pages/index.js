70edab8f05e574051b0ebb8bd5af7a07141b9332
magik99 <lukaszmilewski99@gmail.com>
2020-08-21 23:55:37 +0200

    import fix page/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import List from "../components/list/List"
+import List from "../components/list/list"
 import { ContactForm } from "../components/form/contactForm"
 
 const IndexPage = () => (
