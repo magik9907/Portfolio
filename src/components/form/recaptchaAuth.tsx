4ea2ea3467f3808298b502162e603f59b9b5ee67
magik99 <lukaszmilewski99@gmail.com>
2020-09-04 20:38:09 +0200

    form validation

--- src/components/form/recaptchaAuth.tsx
+++ src/components/form/recaptchaAuth.tsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
-export default function RecaptchaAuth() {
+export default function RecaptchaAuth(props) {
+    const [isChecked, setIsChecked] = useState(false);
     var onloadCallback = function () {
-        alert("grecaptcha is ready!");
+        alert("recaptcha is ready!");
     };
 
     function onChange(value) {
-        console.log("Captcha value:", value);
+        console.log('recaptach change: ' + value);
+        let status = value?true:false;
+        setIsChecked(status);
+        props.onChange(status,"recaptcha");  
     }
 
     return (
-        <div className="recaptcha input-row">
+        <div className={(isChecked)? "checked recaptcha input-row " :" recaptcha input-row "}>
             {/* <div className="g-recaptcha" data-sitekey="6LcIG8IZAAAAAHYcJXDxiEEYVN9MF6Sft25RMAL5"></div> */}
-            <ReCAPTCHA sitekey="6LcIG8IZAAAAAHYcJXDxiEEYVN9MF6Sft25RMAL5" onChange={onChange}  onLoad={onloadCallback}/>
+            <ReCAPTCHA sitekey="6LcIG8IZAAAAAHYcJXDxiEEYVN9MF6Sft25RMAL5" onChange={onChange} onLoad={onloadCallback} />
         </div>)
 }
