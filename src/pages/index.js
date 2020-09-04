cd7c818bc5d0db0c254ebdf883679780d61cb802
magik9907 <43967017+magik9907@users.noreply.github.com>
2020-09-04 20:46:16 +0200

    form fix #1 action del

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ const IndexPage = () => {
         <section className="contact" id="contact">
           <h3>Kontakt</h3>
           <div>
-            <ContactForm method="POST" action="/success" ></ContactForm>
+            <ContactForm method="POST" ></ContactForm>
 
           </div>
           <div className="social">
